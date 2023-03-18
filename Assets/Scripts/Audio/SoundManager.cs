81aad5cdb8c3ad6e32f55422faf8e4f5ef327166
Daniel Giani <dnsgiani@gmail.com>
2023-03-18 00:16:05 -0300

    Implemented pause and unpause sounds
    pause on "p" and unpauses on "o"

--- Assets/Scripts/Audio/SoundManager.cs
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -9,6 +9,52 @@ public class SoundManager : Singleton<SoundManager>
     public List<SFXSetup> SFXSetups;
 
     public AudioSource musicSource;
+    private bool isPaused;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayMusicByType(MusicType.TYPE_01);
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            PauseAllSounds();
+            isPaused = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.O))
+        {
+            if (isPaused)
+            {
+                UnPauseAllSounds();
+                isPaused = false;
+            }
+        }
+    }
+
+    public void PauseAllSounds()
+    {
+        // Pausa a música
+        musicSource.Pause();
+
+        // Pausa todos os efeitos sonoros
+        foreach (var sfxSource in FindObjectsOfType<AudioSource>())
+        {
+            sfxSource.Pause();
+        }
+    }
+
+    public void UnPauseAllSounds()
+    {
+        // Despausa a música
+        musicSource.UnPause();
+
+        // Despausa todos os efeitos sonoros
+        foreach (var sfxSource in FindObjectsOfType<AudioSource>())
+        {
+            sfxSource.UnPause();
+        }
+    }
 
     public void PlayMusicByType(MusicType musicType)
     {
